d3710f766f0340252f47c9eeab82ba079643287f
Megan Nadkarni <megan.nadkarni@gmail.com>
2022-06-21 21:36:54 -0700

    typo

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 import logo from "../public/assets/logo.png";
 import styles from "./footer.module.css";
 
-export default function NavBar() {
+export default function Footer() {
   return (
     <footer className={styles.footer}>
       <section className={styles.subnav}>
